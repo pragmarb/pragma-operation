67d0ce93c0c1ddf22db593dac92fcaf6f930443e
Alessandro Desantis <desa.alessandro@gmail.com>
2016-12-27 22:22:16 +0100

    Fix load order

--- lib/pragma/operation.rb
+++ lib/pragma/operation.rb
@@ -2,10 +2,10 @@
 require 'interactor'
 
 require 'pragma/operation/version'
-require 'pragma/operation/base'
 require 'pragma/operation/authorization'
 require 'pragma/operation/validation'
 require 'pragma/operation/decoration'
+require 'pragma/operation/base'
 
 module Pragma
   # Operations provide business logic encapsulation for your JSON API.
