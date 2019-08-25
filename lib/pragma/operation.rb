43e96e2eca4fbf7a07cfb75c7126497619226673
Alessandro Desantis <desa.alessandro@gmail.com>
2019-08-25 18:32:16 +0200

    Switch to Zeitwerk for autoloading

--- lib/pragma/operation.rb
+++ lib/pragma/operation.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
 require 'json'
-
 require 'trailblazer/operation'
+require 'zeitwerk'
 
-require 'pragma/operation/version'
-require 'pragma/operation/base'
-require 'pragma/operation/error'
-
-require 'pragma/operation/response'
-require 'pragma/operation/response/bad_request'
-require 'pragma/operation/response/not_found'
-require 'pragma/operation/response/forbidden'
-require 'pragma/operation/response/unprocessable_entity'
-require 'pragma/operation/response/created'
-require 'pragma/operation/response/ok'
-require 'pragma/operation/response/no_content'
-require 'pragma/operation/response/unauthorized'
-require 'pragma/operation/response/service_unavailable'
-require 'pragma/operation/response/internal_server_error'
-require 'pragma/operation/response/conflict'
-require 'pragma/operation/response/payment_required'
+Zeitwerk::Loader.new.tap do |loader|
+  loader.tag = File.basename(__FILE__, '.rb')
+  loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
+  loader.push_dir(File.expand_path('..', __dir__))
+  loader.setup
+end
 
 module Pragma
   # Operations provide business logic encapsulation for your JSON API.
--- pragma-operation.gemspec
+++ pragma-operation.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'trailblazer-operation', '~> 0.4.1'
+  spec.add_dependency 'zeitwerk', '~> 2.1'
 
   spec.add_development_dependency 'bundler'
   spec.add_development_dependency 'rake'
